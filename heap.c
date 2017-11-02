7a4efa627d0a78be0fc052ac2091baef5e7e3541
Lucasark <Lucas.arkantos@hotmail.com>
2017-11-02 15:45:55 -0200

    1.0.0

--- heap.c
+++ heap.c
@@ -5,10 +5,11 @@ int minValor(Heap h){
 }
 void insereValor(Heap *h, int n){ // TESTE n = 2 i = 1 -- n = 3 i =2 
 	int i, aux, pai;
+	h->last++;//Da espaço para mais um futuro elemento
 	h->elementos[h->last] = n;
-	i = h->last;// i = 1
-	pai = i/2; //pai = 0
-	while(pai>0 && i>0){ //F & T
+	i = h->last;// TESTE i = 1
+	pai = i/2; //TESTE pai = 0
+	while(pai>0 && i>0){ //TESTE F & T
 		if (h->elementos[i] < h->elementos[pai]){ //Troca de pai e filho
 			aux = h->elementos[pai];
 			h->elementos[pai]=h->elementos[i];
@@ -17,16 +18,15 @@ void insereValor(Heap *h, int n){ // TESTE n = 2 i = 1 -- n = 3 i =2
 		i = pai; //Atualziado quem é o filho
 		pai = i/2;//Atualliza quem é pai
 	}
-	h->last++;//Da espaço para mais um elemento
 	return;
 }
-void criaHeap(Heap *h){ //Inicia o vetor om indice 1
-	h->last = 1;
+void criaHeap(Heap *h){ //Inicia o vetor om indice 0
+	h->last = 0;
 	return;
 }
 void imprimeHeap(Heap h){
-	for (int i=1; i<=(h.last - 1); i++){//Não acessar o vetor "previmente" criado
-		printf("%d - ", h.elementos[i]); // 2 - 3
+	for (int i=1; i<=(h.last); i++){//Não acessar o vetor "previmente" criado
+		printf("%d - ", h.elementos[i]); // TESTE 2 - 3
 	}
 	return;
 }
